db56e06972e7fe374a7829494c8ceba5bc07bfab
studstone <nikolas2996@gmail.com>
2022-09-09 21:11:04 +0300

    create homepage

---
+++ src/pages/HomePage/HomePages.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Products from '../../components/Products/Products';
+
+const HomePages = () => (
+    <>
+        <Products />
+    </>
+);
+
+export default HomePages;
